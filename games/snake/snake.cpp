beee915ff3c22e7f719c649b32d284a208455257
Davy <davy.hantz@epitech.eu>
2018-03-23 10:55:32 +0100

    Snake

---
+++ games/snake/snake.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+** snake.cpp
+*/
+
+#include "snake.hpp"
---
+++ games/snake/snake.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+** snake.hpp
+*/
+
+#ifndef CPP_ARCADE_SNAKE_HPP
+#define CPP_ARCADE_SNAKE_HPP
+
+#endif //CPP_ARCADE_SNAKE_HPP

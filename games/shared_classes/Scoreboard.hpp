76d810eb345ceecb6610b97d075503b58eff61b3
Davy <davy.hantz@epitech.eu>
2018-04-01 23:31:52 +0200

    Scoreboard Initial Commit

---
+++ games/shared_classes/Scoreboard.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+** Scoreboard.hpp
+*/
+
+#ifndef CPP_ARCADE_SCOREBOARD_HPP
+#define CPP_ARCADE_SCOREBOARD_HPP
+
+
+class Scoreboard {
+
+};
+
+
+#endif //CPP_ARCADE_SCOREBOARD_HPP
